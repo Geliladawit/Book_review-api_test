b783b59b384344edf500dd69740a589d8fb1a84c
Gelila Dawit <geliladawit050@gmail.com>
2024-11-20 20:00:36 +0300

    super user script added

--- pages/tests.py
+++ pages/tests.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib.auth import get_user_model
 from django.test import SimpleTestCase, TestCase
 from django.urls import reverse
@@ -34,40 +33,3 @@ class SignupPageTests(TestCase):
                     [0].username, self.username)
         self.assertEqual(get_user_model().objects.all()
                     [0].email, self.email)
-=======
-from django.contrib.auth import get_user_model
-from django.test import SimpleTestCase, TestCase
-from django.urls import reverse
-class HomePageTests(SimpleTestCase):
-    def test_home_page_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-    def test_view_url_by_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
-class SignupPageTests(TestCase):
-    username = 'newuser'
-    email = 'newuser@email.com'
-    def test_signup_page_status_code(self):
-        response = self.client.get('/users/signup/')
-        self.assertEqual(response.status_code, 200)
-    def test_view_url_by_name(self):
-        response = self.client.get(reverse('signup'))
-        self.assertEqual(response.status_code, 200)
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('signup'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'signup.html')
-    def test_signup_form(self):
-        new_user = get_user_model().objects.create_user(
-            self.username, self.email)
-        self.assertEqual(get_user_model().objects.all().count(), 1)
-        self.assertEqual(get_user_model().objects.all()
-                    [0].username, self.username)
-        self.assertEqual(get_user_model().objects.all()
-                    [0].email, self.email)
->>>>>>> c3c8c527380597cc48d194b1529139671bd20a31
