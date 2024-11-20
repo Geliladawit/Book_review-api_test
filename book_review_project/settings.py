a7e27fc219a8e64b65fa572812cbebf7b1d89c2d
Gelila Dawit <111200319+Geliladawit@users.noreply.github.com>
2024-11-20 14:07:22 +0300

    Update settings.py

--- book_review_project/settings.py
+++ book_review_project/settings.py
@@ -80,15 +80,15 @@ WSGI_APPLICATION = 'book_review_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+}
 
 
 # Password validation
