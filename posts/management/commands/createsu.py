a069282b9c2f114bf566113f92cea27939c38dbb
Gelila Dawit <geliladawit050@gmail.com>
2024-11-21 15:23:48 +0300

    done

--- posts/management/commands/createsu.py
+++ posts/management/commands/createsu.py
@@ -1,21 +1,25 @@
 from pathlib import Path
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 from dotenv import load_dotenv
 import os 
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
-load_dotenv(BASE_DIR/ '.env')
+load_dotenv(BASE_DIR / '.env')
 
 class Command(BaseCommand):
     help = 'Creates a superuser.'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='SUPERUSER_USERNAME').exists():
+        User = get_user_model()
+
+        if not User.objects.filter(username=os.getenv('SUPERUSER_USERNAME')).exists():
             User.objects.create_superuser(
-                username = os.getenv('SUPERUSER_USERNAME'),
-                email = os.getenv('SUPERUSER_EMAIL'),
-                password = os.getenv('SUPERUSER_PASSWORD')
+                username=os.getenv('SUPERUSER_USERNAME'),
+                email=os.getenv('SUPERUSER_EMAIL'),
+                password=os.getenv('SUPERUSER_PASSWORD')
             )
-        print('Superuser has been created.')
+            self.stdout.write(self.style.SUCCESS('Superuser has been created.'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists.'))
